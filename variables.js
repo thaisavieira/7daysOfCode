0410b2b980a3cf1efca005c9473159ef41af2e38
Thaísa Vieira <thaisavieira.tech@proton.me>
2024-08-01 09:10:13 -0300

    day 02 - variables exercise

---
+++ variables.js
@@ -0,0 +1,13 @@
+let name = prompt('Qual o seu nome?')
+let age = prompt('Quantos anos você tem?')
+let language =prompt('Qual linguagem de programação você está estudando?')
+let answer = 'Olá ' + ', você tem ' + age + 'e já estou aprendendo ' + language + '.'
+
+alert(answer)
+
+let question = prompt('Você gosta de estudar ' + linguagem + 'Responda com 1 para SIM ou 2 para NÃO.')
+if (question == 1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.")
+    } else {
+    alert('Ahh que pena... Já tentou aprender outras linguagens?')
+}
